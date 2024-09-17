3cd87590b01305206ca9749045a2d97157d7125f
Ursula Das <ursuladas17309@gmail.com>
2024-09-17 19:24:54 -0400

    added auroc,auprc,accuracy,iou,mse as metrics

--- test.py
+++ test.py
@@ -3,6 +3,10 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader
+from torchmetrics import AUROC, AveragePrecision, F1Score
+from torchmetrics.segmentation import MeanIoU
+from torchmetrics.classification.accuracy import Accuracy
+from torchmetrics.regression import MeanSquaredError
 import torch.nn.functional as F
 from tqdm import tqdm
 import segmentation_models_pytorch as smp
@@ -11,9 +15,9 @@ import xarray as xr
 import pickle
 import matplotlib.pyplot as plt
 # Import models from models.py
-from seg_models import UnetModel, FPNModel, PSPNetModel, DeepLabV3Model, PANModel
+from seg_models import UnetModel, FPNModel, PSPNetModel, DeepLabV3Model, PANModel,UnetPlusPlusModel,MAnetModel,DeepLabV3PlusModel
 import cv2
-from qual_metrics import compute_centroid_mse
+from qual_metrics import compute_centroid_mse,compute_area_mse
 
 # Import your dataset class
 from datamodule import SegmentationDataset  # Replace with your actual dataset class
@@ -23,6 +27,16 @@ def evaluate(model, loader, criterion,device):
     model.eval()
     val_losses=[]
     centroid_mse_list=[]
+
+    # Initialize metrics
+    auroc = AUROC(task="binary").to(device)
+    auprc = AveragePrecision(task="binary").to(device)
+    f1 = F1Score(task="binary").to(device)
+    accuracy = Accuracy(task="binary").to(device)
+    miou = MeanIoU(num_classes=2).to(device)  # For binary IoU
+    mse=MeanSquaredError().to(device)
+
+
     with torch.no_grad():
         for x_val, y_val in loader:
             x_val = x_val.to(device)
@@ -31,25 +45,52 @@ def evaluate(model, loader, criterion,device):
             # Forward pass
             predictions = model(x_val)
             loss = criterion(predictions, y_val)
+
+            mse.update(predictions,y_val)
+
             predictions=torch.nn.functional.sigmoid(predictions)
+
+            # Update metrics
+            auroc.update(predictions, y_val.int())
+            auprc.update(predictions, y_val.int())
+            f1.update(predictions, y_val.int())
+            
+            
+
+
             threshold = 0.3
             predictions = (predictions > threshold).float()
             val_losses.append(loss.item())
 
             # Compute centroid MSE for the batch
+            miou.update(predictions.int(), y_val.int())
+            accuracy.update(predictions, y_val.int())
             centroid_mse = compute_centroid_mse(predictions, y_val)
+            area_mse=compute_area_mse(predictions,y_val)
             if centroid_mse is not None:
                 centroid_mse_list.append(centroid_mse)
-
+            # print(f'{predictions} {y_val} {miou.compute()}')
 
     avg_loss = sum(val_losses) / len(val_losses)
     avg_centroid_mse = sum(centroid_mse_list) / len(centroid_mse_list) if len(centroid_mse_list) > 0 else None
+    avg_area_mse = sum(area_mse) / len(area_mse)
 
-       # Save the predictions and ground truths
-    # torch.save(predictions, 'test_predictions.pt')
-    # torch.save(y_val, 'test_ground_truth.pt')
+  # Compute the final values of the metrics
+    final_auroc = auroc.compute()
+    final_auprc = auprc.compute()
+    final_f1 = f1.compute()
+    final_accuracy = accuracy.compute()
+    final_iou = miou.compute()
+
+    # Reset metrics to clear for the next evaluation
+    auroc.reset()
+    auprc.reset()
+    f1.reset()
+    accuracy.reset()
+    miou.reset()
+
     
-    return predictions,avg_loss,avg_centroid_mse
+    return predictions, avg_loss, avg_centroid_mse, avg_area_mse, final_auprc, final_auroc, final_f1, final_accuracy, final_iou
 
 def visualize_predictions(pred_path, gt_path, batch_idx=0):
 
@@ -112,9 +153,9 @@ def main(args):
     test_loader = DataLoader(test_dataset, batch_size=args.batch_size,shuffle=False)
     
     # Instantiate the model
-    model_cls = {"Unet": UnetModel, "FPN": FPNModel, "PSPNet": PSPNetModel, "DeepLabV3": DeepLabV3Model, "PAN": PANModel}
+    model_cls = {"Unet": UnetModel, "FPN": FPNModel, "PSPNet": PSPNetModel, "DeepLabV3": DeepLabV3Model, "PAN": PANModel, "Unet++":UnetPlusPlusModel,"MAnet":MAnetModel,"DeepLabV3+":DeepLabV3Model}
     model = model_cls[args.model_name](encoder_name=args.encoder_name, in_channels=len(args.input_vars), classes=1)
-    model.load_state_dict(torch.load('/home/udas/Desktop/UD_Data_Copy/Segmentation_Models/models/best_model.pth'))
+    model.load_state_dict(torch.load('/home/udas/Desktop/UD_Data_Copy/Segmentation_Models/models/best_model_Unet++.pth',weights_only=True))
     model=model.to(device)
 
     # Loss and optimizer
@@ -130,10 +171,10 @@ def main(args):
     elif loss=='ts':
         criterion=smp.losses.TverskyLoss(mode='binary', classes=1, log_loss=False, from_logits=True,eps=1e-07, alpha=0.3, beta=0.7, gamma=1.0)[source]
 
-    test_prediction,test_epoch_loss,test_centroid_mse = evaluate(model, test_loader, criterion,device)
-    print(f"Test Loss: {test_epoch_loss:.6f} and Test Centroid {test_centroid_mse}")
+    test_prediction,test_epoch_loss,test_centroid_mse,test_area_mse,final_auprc, final_auroc, final_f1, final_accuracy, final_iou = evaluate(model, test_loader, criterion,device)
+    print(f"Test Loss: {test_epoch_loss:.6f} and Test Centroid {test_centroid_mse} and Test Area MSE {test_area_mse} \n Test AUPRC {final_auprc} \n Test AUROC {final_auroc} \n Test f1 {final_f1} \n Test Accuracy {final_accuracy}, \n Test IoU {final_iou}")
     # Call the visualization function
-    visualize_predictions('./test_predictions.pt', './test_ground_truth.pt', batch_idx=0)
+    # visualize_predictions('./test_predictions.pt', './test_ground_truth.pt', batch_idx=0)
 
     
  
@@ -157,7 +198,7 @@ if __name__ == "__main__":
     parser.add_argument('--batch_size', type=int, default=16, help='Batch size')
     # parser.add_argument('--epochs', type=int, default=25, help='Number of training epochs')
     parser.add_argument('--encoder_name', type=str, default='resnet34', help='Name of the encoder')
-    parser.add_argument('--model_name', type=str, default='Unet', choices=['Unet', 'FPN', 'PSPNet', 'DeepLabV3', 'PAN'], help='Segmentation model to use')
+    parser.add_argument('--model_name', type=str, default='FPN', choices=['Unet', 'FPN', 'PSPNet', 'DeepLabV3', 'PAN','MAnet','Unet++','DeepLabV3+'], help='Segmentation model to use')
 
 
     # Parse arguments
