904b052d708b6eedaccfc8bdca517fdf7fcbb7de
Ursula Das <ursuladas17309@gmail.com>
2024-11-11 18:20:56 -0500

    don't remember what change was made :(

--- datamodule.py
+++ datamodule.py
@@ -3,10 +3,12 @@ import numpy as np
 import xarray as xr
 import torch
 import random
+import pickle
+import pandas as pd
 
 
 class SegmentationDataset(Dataset):
-    def __init__(self, datasets,stat_dict,target,input_vars,crop_size=0,rotate=False,stat='mean_std'):
+    def __init__(self, datasets,stat_dict,target,input_vars,crop_size=0,rotate=False,stat='mean_std',years_modified=False,mode='train'):
         """
         Args:
             datasets (list of xarray datasets): List of monotemporal datasets 
@@ -23,6 +25,16 @@ class SegmentationDataset(Dataset):
         self.target = target
         self.crop_size=crop_size
         self.rotate=rotate
+        self.years_modified=years_modified
+        self.mode=mode
+
+        self.area_path=f'/home/udas/Desktop/UD_Data_Copy/Segmentation_Models/EDA/areas.pkl'
+
+        self.area_list=self.__pickle_load_area__(self.area_path)
+        
+        self.qr=self.__cal_iqr__(self.area_list)
+
+        self.filtered_sample_list=self.__filter_by_size__(self.sample_list,self.qr)
 
         #Uncomment and modify args if using source pkl file
         # self.min_max_dict = min_max_dict
@@ -38,14 +50,105 @@ class SegmentationDataset(Dataset):
             self.std = np.stack([self.stat_dict[var]['std'] for var in input_vars])
 
 
+    def __calc_area__(self,sample_list,area_file):
+
+        area_list=[]
+        with open(area_file,'ab') as f:
+            for idx,sample in enumerate(tqdm(sample_list)):
+                ignition_point=sample['ignition_points'].isel(time=-1).values
+                # print(ignition_point.shape)
+                fire_indices=np.argwhere(ignition_point>0)
+                # print(f' fire indices shape {fire_indices.shape}')
+                if fire_indices.shape[0]>1:
+                    area=0
+                    for idx in fire_indices:
+                        # print(f'Inside multiple ignitions')
+                        row,col=idx
+                        # print(f'area is {area}')
+                        # print(f'ind area is {ignition_point[row,col]}')
+                        # print(f'sum is {area+ignition_point[row,col]}')
+                        area+=ignition_point[row,col]
+                        # print(f'new area is {area}')
+                    area_list.append(area)
+                else:
+                    # print(f'Inside single ignition')
+                    # print(fire_indices)
+                    row,col=fire_indices[0]
+                    # print(row,col)
+                    area=ignition_point[row,col]
+                    # print(f'area is {area}')
+                    area_list.append(area)
+                pickle.dump(area,f)
+
+        return area_list
+
+    def __pickle_load_area__(self,filepath):
+        with open(filepath, 'rb') as f:
+            area_list = []
+            while True:
+                try:
+                    # Load individual areas and append to the list
+                    area_list.append(pickle.load(f))
+                except EOFError:
+                    break
+        return area_list
+
+    def __cal_iqr__(self,area_list):
+        df=pd.DataFrame({'burned_area(hectares)':area_list})
+        q1=df['burned_area(hectares)'].quantile(0.25)
+        q3=df['burned_area(hectares)'].quantile(0.75)
+        iqr=q3-q1
+        print(f'range {q1-1.5*iqr} to {q3+1.5*iqr}')
+
+        return q1,q3,iqr
+
+    def __filter_by_size__(self,sample_list,qr):
+        modified_list=[]
+        q1,q3,iqr=qr
+        upper_bound=q3+1.5*iqr
+        lower_bound=q1-1.5*iqr
+        for sample in sample_list:
+            ignition_point=sample['ignition_points'].isel(time=-1).values
+            # print(ignition_point.shape)
+            fire_indices=np.argwhere(ignition_point>0)
+            # print(f' fire indices shape {fire_indices.shape}')
+            if fire_indices.shape[0]>1:
+                area=0
+                for idx in fire_indices:
+                    # print(f'Inside multiple ignitions')
+                    row,col=idx
+                    # print(f'area is {area}')
+                    # print(f'ind area is {ignition_point[row,col]}')
+                    # print(f'sum is {area+ignition_point[row,col]}')
+                    area+=ignition_point[row,col]
+                    # print(f'new area is {area}')
+            else:
+                # print(f'Inside single ignition')
+                # print(fire_indices)
+                row,col=fire_indices[0]
+                # print(row,col)
+                area=ignition_point[row,col]
+                # print(f'area is {area}')
+
+            if lower_bound<=area<=upper_bound:
+                modified_list.append(sample)
+
+        return modified_list
+
+
     def __len__(self):
         # Return Number of Samples in the Dataset
-        return len(self.sample_list)
+        return len(self.filtered_sample_list)
 
     def __getitem__(self,idx):
 
         #fetch a single netcdf file dataset
-        sample = self.sample_list[idx].isel(time=-1) #(64,64,Number of Features)
+        if 'time' in self.filtered_sample_list[idx].dims:
+            sample = self.filtered_sample_list[idx].isel(time=-1) #(64,64,Number of Features)
+        else:
+            print(f'time missing')
+            sample = self.filtered_sample_list[idx]
+
 
         #make ignition point values binary
         sample['ignition_points'] = sample['ignition_points'].where(sample['ignition_points'] == 0, 1)
@@ -131,3 +234,6 @@ class SegmentationDataset(Dataset):
         # print(f'final inputs shape  and type is {type(inputs)} and {inputs.shape} \n  final targets shape and type is {type(target)}  and {target.shape}')
         return inputs,target
     
+    # def main():
+        
+    # if __name__ == "__main__":
