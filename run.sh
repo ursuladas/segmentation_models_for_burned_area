7e36b81d4cd2f80f091ea1eef1cf653972ec1413
Ursula Das <ursuladas17309@gmail.com>
2024-11-11 18:26:57 -0500

    added and removed new argumetns

--- run.sh
+++ run.sh
@@ -7,29 +7,37 @@ SCRIPT_PATH="/home/udas/Desktop/UD_Data_Copy/Segmentation_Models/train.py"
 ROOT_DIR="/home/udas/Desktop/UD_Data_Copy/b.final_burned_area"
 
 # Paths to the training and validation statistics pickle files
-TRAIN_STATS="/home/udas/Desktop/UD_Data_Copy/Spatial_Models/train_stats.pkl"
-VAL_STATS="/home/udas/Desktop/UD_Data_Copy/Spatial_Models/val_stats.pkl"
+TRAIN_STATS="/home/udas/Desktop/UD_Data_Copy/Spatial_Models/graph_class_scripts/stat_dicts/train_stats.pkl"
+VAL_STATS="/home/udas/Desktop/UD_Data_Copy/Spatial_Models/graph_class_scripts/stat_dicts/val_stats.pkl"
 
 # Input variables and target variable
-INPUT_VARS="ignition_points ssrd smi d2m t2m wind_speed"
+# INPUT_VARS="ignition_points ssrd smi d2m t2m wind_speed wind_direction slope aspect"
+INPUT_VARS="ignition_points ndvi roads_distance slope smi lst_day lst_night sp t2m wind_direction wind_speed lc_agriculture lc_forest lc_grassland lc_settlement lc_shrubland lc_sparse_vegetation"
 TARGET="burned_areas"
+STAT="min_max"
 
 # Model configuration
 MODEL_NAME="DeepLabV3"
-ENCODER_NAME="resnet34"
+ENCODER_NAME="efficientnet-b7"
 
 # Training parameters
-TRAIN_YEARS="2017 2018 2019 2020"
-VAL_YEARS="2022"
+TRAIN_YEARS="2006 2007 2008 2009 2010 2011 2012 2013 2014 2015 2016 2017 2018 2019"
+# TRAIN_YEARS="2006"
+
+VAL_YEARS="2020"
+# VAL_YEARS="2007"
+
 CROP_SIZE=32
-BATCH_SIZE=16
-EPOCHS=25
-LEARNING_RATE=1e-4
+BATCH_SIZE=128
+EPOCHS=200
+LEARNING_RATE=0.001
+WEIGHT_DECAY=0.0005
 LOSS="BCE_raw"
-ROTATE=True
+ROTATE="--rotate"
 # Run the Python script with the specified arguments
 python3 "$SCRIPT_PATH" \
   --root_dir "$ROOT_DIR" \
+  --stat "$STAT"\
   --train_stat_dict "$TRAIN_STATS" \
   --val_stat_dict "$VAL_STATS" \
   --input_vars $INPUT_VARS \
@@ -42,6 +50,7 @@ python3 "$SCRIPT_PATH" \
   --batch_size "$BATCH_SIZE" \
   --epochs "$EPOCHS" \
   --learning_rate "$LEARNING_RATE" \
+  --weight_decay "$WEIGHT_DECAY" \
   --loss "$LOSS" \
-  --desc "$MODEL_NAME"+"$LOSS"  \
-  --rotate $ROTATE
+  --desc "$MODEL_NAME"+"$LOSS"+"$ENCODER_NAME"  \
+  # $ROTATE
